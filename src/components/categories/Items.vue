faae254aa81f9cdc6eb2c83e9b5a8a381601ef9b
wildan1112 <wildanmalulazam@gmail.com>
2022-12-10 10:54:24 +0700

    sesi12 m58

--- src/components/categories/Items.vue
+++ src/components/categories/Items.vue
@@ -26,10 +26,18 @@ onMounted(() => {
 
 <template>
   <div class="container px-4 mx-auto my-16 md:px-12">
-    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">{{ category.name }}</h2>
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">
+      {{ category.name }}
+    </h2>
     <div class="flex flex-wrap -mx-1 lg:-mx-4">
-      <ItemCard v-for="item in items" :key="item.id" :id="item.id" :title="item.name" :description="item.subtitle"
-        :image="item.thumbnails" />
+      <ItemCard
+        v-for="item in items"
+        :key="item.id"
+        :id="item.id"
+        :title="item.name"
+        :description="item.subtitle"
+        :image="item.thumbnails"
+      />
     </div>
   </div>
 </template>
